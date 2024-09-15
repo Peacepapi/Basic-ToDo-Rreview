0e82407943c7c4c1443d29f617bde8fee8a247c1
Peacepapi <khmaipapi@yahoo.com>
2024-09-15 14:30:48 -0400

    Added fonts and create basic rfc + css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// react code gets compile and display in the root id element
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
